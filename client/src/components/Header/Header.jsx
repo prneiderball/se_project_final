6f3c8ad3e098d03505c0cc82547d8e4d0818a64e
P.R. Neider-Ball <phillrballn@icloud.com>
2025-11-22 13:15:06 -0600

    newStyle(header): updated padding, styled buttons

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,16 +4,10 @@ import newsExplorerLogo from "../../assets/newsExplorerLogo.svg";
 function Header() {
   return (
     <header className="header">
-      <img
-        className="header__logo"
-        src={newsExplorerLogo}
-        alt="News Explorer Logo"
-      />
+      <img className="header__logo" src={newsExplorerLogo} alt="News Explorer Logo" />
       <div className="header__navigation">
         <button className="header__button header__button--home">Home</button>
-        <button className="header__button header__button--signin">
-          Sign In
-        </button>
+        <button className="header__button header__button--signin">Sign In</button>
       </div>
     </header>
   );
