5eb34739709da5807b8d0d4e9e6db7e159abbbc0
P.R. Neider-Ball <phillrballn@icloud.com>
2025-11-30 09:20:56 -0600

    Update App.jsx

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import Header from "../Header/Header.jsx";
 import Hero from "../Hero/Hero.jsx";
 import About from "../About/About.jsx";
-import Results from "../Results/Results.jsx";
-import NotFound from "../NotFound/NotFound.jsx";
 
 function App() {
   return (
