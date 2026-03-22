0e70af1dc299cf9d1a4ae11eabdf0ace9867773f
prneiderball <phillrballn@icloud.com>
2026-03-22 07:03:10 -0500

    Update newsApi.js

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -1,9 +1,12 @@
 const baseUrl =
-    process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? "https://nomoreparties.co/news/v2/everything"
     : "https://newsapi.org/v2/everything";
 export function searchNews(keyword) {
-  const apiKey = import.meta.env.VITE_API_KEY;
+  // NOTE: API key is included for demo purposes only.
+  // In production, this will be handled via backend.
+  const apiKey =
+    import.meta.env.VITE_API_KEY || "3bac86ff448446fbb3dc6f4674a1e448";
 
   const today = new Date();
   const fromDate = new Date();
